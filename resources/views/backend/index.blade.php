4aa980324ff34567fe3068f768d193a5685318b2
laptrinhkalzen <tiendat98274@gmail.com>
2021-02-23 16:46:47 +0700

    luong 23 2 21

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -25,18 +25,18 @@
                 </div>
             </a>
         </div>
-        <div class="col-sm-4">
-            <a href="{{route('admin.contact.index')}}">
+        <!-- <div class="col-sm-4">
+            <a href="">
                 <div class="tile-stats tile-green"> 
                     <div class="icon"><i class="icon-bubble-dots4"></i>
                     </div> 
                     <div class="num" data-start="0" data-end="0" data-postfix="" data-duration="1500" data-delay="0">
-                        {{$contact_count}}
+                        
                     </div>
                     <h3>Liên hệ</h3>
                 </div>
             </a>
-        </div>
+        </div> -->
     </div>
 </div>
 <!-- /content area -->
